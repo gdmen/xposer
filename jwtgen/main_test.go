fc0a3f8f06b14f667fcbed95ee57cdb8279168af
gdmen <gmenezes@alumni.upenn.edu>
2020-09-29 11:52:16 -0700

    Update jwtgen test.

--- jwtgen/main_test.go
+++ jwtgen/main_test.go
@@ -7,44 +7,48 @@ import (
 	"garymenezes.com/xfinity-xposer/test"
 )
 
+const (
+	TESTCMD = "../bin/jwtgen"
+)
+
 func TestHelp(t *testing.T) {
-	stdout, stderr, err := test.RunCommand("../bin/jwtgen", []string{"-h"})
+	stdout, stderr, err := test.RunCommand(TESTCMD, []string{"-h"})
 	if err == nil || err.Error() != "exit status 2" || !strings.Contains(stderr.String(), "Usage of") {
 		t.Error(stdout.String(), stderr.String(), err)
 	}
 }
 
-func TestMissingArgs(t *testing.T) {
-	stdout, stderr, err := test.RunCommand("../bin/jwtgen", nil)
+func TestFlags(t *testing.T) {
+	stdout, stderr, err := test.RunCommand(TESTCMD, nil)
 	if err == nil || err.Error() != "exit status 1" || !strings.Contains(stdout.String(), "you must enter a private key path") {
 		t.Error(stdout.String(), stderr.String(), err)
 	}
 
-	stdout, stderr, err = test.RunCommand("../bin/jwtgen", []string{"-d", "device"})
+	stdout, stderr, err = test.RunCommand(TESTCMD, []string{"-d", "device"})
 	if err == nil || err.Error() != "exit status 1" || !strings.Contains(stdout.String(), "you must enter a private key path") {
 		t.Error(stdout.String(), stderr.String(), err)
 	}
 
-	stdout, stderr, err = test.RunCommand("../bin/jwtgen", []string{"-d", "device", "-o", "../bin/test.jwt"})
+	stdout, stderr, err = test.RunCommand(TESTCMD, []string{"-d", "device", "-o", "../bin/test.jwt"})
 	if err == nil || err.Error() != "exit status 1" || !strings.Contains(stdout.String(), "you must enter a private key path") {
 		t.Error(stdout.String(), stderr.String(), err)
 	}
 }
 
 func TestBadKeys(t *testing.T) {
-	stdout, stderr, err := test.RunCommand("../bin/jwtgen", []string{"-d", "device", "-o", "../bin/test.jwt", "-k", "../test/test.pub"})
+	stdout, stderr, err := test.RunCommand(TESTCMD, []string{"-d", "device", "-o", "../bin/test.jwt", "-k", "../test/test.pub"})
 	if err == nil || err.Error() != "exit status 1" {
 		t.Error(stdout.String(), stderr.String(), err)
 	}
 
-	stdout, stderr, err = test.RunCommand("../bin/jwtgen", []string{"-d", "device", "-o", "../bin/test.jwt", "-k", "main.go"})
+	stdout, stderr, err = test.RunCommand(TESTCMD, []string{"-d", "device", "-o", "../bin/test.jwt", "-k", "main.go"})
 	if err == nil || err.Error() != "exit status 1" || !strings.Contains(stdout.String(), "Invalid Key") {
 		t.Error(stdout.String(), stderr.String(), err)
 	}
 }
 
 func TestSuccess(t *testing.T) {
-	stdout, stderr, err := test.RunCommand("../bin/jwtgen", []string{"-d", "device", "-o", "../bin/test.jwt", "-k", "../test/test.pem"})
+	stdout, stderr, err := test.RunCommand(TESTCMD, []string{"-d", "device", "-o", "../bin/test.jwt", "-k", "../test/test.pem"})
 	if err != nil {
 		t.Error(stdout.String(), stderr.String(), err)
 	}
